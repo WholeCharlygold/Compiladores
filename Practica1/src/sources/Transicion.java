4a320a2cbc5de1a5f27f86d94010d22b324978cd
McGilfordJose <chucho970407@gmail.com>
2019-08-19 09:18:03 -0500

    netbeans

---
+++ Practica1/src/sources/Transicion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sources;
+
+/**
+ *
+ * @author chuch
+ */
+public class Transicion {
+    private char c;
+    private Estado origen= new Estado();
+    private Estado destino= new Estado();
+
+    public void set(char c,Estado origen, Estado destino) {
+        this.c = c; 
+        this.origen = origen;
+        this.destino = destino;
+    }
+
+    public char getC() {
+        return c;
+    }
+
+    public int getOrigen() {
+        return origen.getId();
+    }
+
+    public int getDestino() {
+        return destino.getId();
+    }
+
+
+
+
+
+
+    
+    
+}
