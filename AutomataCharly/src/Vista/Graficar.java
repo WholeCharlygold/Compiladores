cce78a9ccf98a4d91083b619a70d6aed9c0b74dc
McGilfordJose <chucho970407@gmail.com>
2019-09-04 09:23:00 -0500

    Vista

---
+++ AutomataCharly/src/Vista/Graficar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author chuch
+ */
+public class Graficar {
+    public void pain(Graphics g)
+    {
+        g.drawLine(10, 10, 10, 10);
+    }
+}
